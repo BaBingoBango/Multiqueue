4d255b7866525d70116ec27881d8c531f32de12b
Ethan Marshall <ethanm265@yahoo.com>
2022-08-01 10:09:19 -0400

    Added the QueueSong.swift file!

---
+++ PartyQueue/Models/QueueSong.swift
@@ -0,0 +1,20 @@
+//
+//  QueueSong.swift
+//  PartyQueue
+//
+//  Created by Ethan Marshall on 7/31/22.
+//
+
+import Foundation
+import MusicKit
+import CloudKit
+
+/// A song that was added to an Apple Music Queue via a Multiqueue room.
+struct QueueSong {
+    var ID = UUID()
+    var song: Song
+    var playType: PlayType
+    var adderName: String
+    var timeAdded: Date
+    var artwork: CKAsset
+}
